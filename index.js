e13856bdaf0b70c5cf9e729870a4e4ed2f57116d
dsadsadsss <130039988+dsadsadsss@users.noreply.github.com>
2023-12-10 14:36:34 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const { exec } = require('child_process');
+const { spawn } = require('child_process');
 
 const command = 'chmod +x ./start.sh && ./start.sh';
 
-exec(command, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error: ${error.message}`);
-    return;
-  }
-  
-  if (stderr) {
-    console.error(`stderr: ${stderr}`);
-    return;
-  }
-
-  console.log(`stdout: ${stdout}`);
+const childProcess = spawn(command, {
+  shell: true,
+  stdio: 'inherit' // 使用 'inherit' 来共享子进程的输入输出流
 });
 
-
+childProcess.on('exit', (code, signal) => {
+  if (code !== null) {
+    console.log(`子进程退出，退出码: ${code}`);
+  } else if (signal !== null) {
+    console.error(`子进程被终止，信号: ${signal}`);
+  }
+});
